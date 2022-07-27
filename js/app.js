3c8e786504cb24936856eac34ae109df14ec21b4
chuckicode <mag3davis@gmail.com>
2022-07-27 17:29:06 +0200

    made responsive chandes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
-var submitBtn = document.getElementById('submitBtn').addEventListener('click', function addWarning () {
-var x = document.getElementById('warnigBox');
-x.style.display='block';
-})
+// var submitBtn = document.getElementById('submitBtn').addEventListener('click', function addWarning () {
+// var x = document.getElementById('warnigBox');
+// x.style.display='block';
+// })
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
